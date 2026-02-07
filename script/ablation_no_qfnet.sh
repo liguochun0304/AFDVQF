bdcc9c42c5219688e09754d26fb0f3b6457831d3
liguochun0304 <liguochun0304@163.com>
2026-02-07 22:32:39 +0800

    add ablation

--- script/ablation_no_qfnet.sh
+++ script/ablation_no_qfnet.sh
@@ -1,16 +1,16 @@
-python3 ../train.py \
-  --dataset_name twitter2015 \
-  --device cuda:0 \
-  --ex_name ablation_no_qfnet \
-  --save_name ablation_no_qfnet_twitter2015 \
-  --use_qfnet false \
-  --use_adaptive_fusion true
+# python3 ../train.py \
+#   --dataset_name twitter2015 \
+#   --device cuda:0 \
+#   --ex_name ablation_no_qfnet \
+#   --save_name ablation_no_qfnet_twitter2015 \
+#   --use_qfnet false \
+#   --use_adaptive_fusion true
 
-python3 ../test.py \
-  --save_name ablation_no_qfnet_twitter2015 \
-  --device cuda:0 \
-  --split test \
-  --record_file logs/ablation_records.csv
+# python3 ../test.py \
+#   --save_name ablation_no_qfnet_twitter2015 \
+#   --device cuda:0 \
+#   --split test \
+#   --record_file logs/ablation_records.csv
 
 python3 ../train.py \
   --dataset_name twitter2017 \
